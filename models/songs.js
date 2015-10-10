45c92ed237f15c1a8d6b20005a0e43e1d33536c0
Neel Kowdley <nkowdley@gmail.com>
2015-10-09 23:12:14 -0400

    update models

--- models/songs.js
+++ models/songs.js
@@ -2,11 +2,8 @@ var mongoose = require('mongoose');
 
 var schema = new mongoose.Schema({
   //schema setup
-  activityName: String,
-  lat: String,
-  lng: String,
-  time: String,
-  numberofPeople:Number,
+  name: String,
+  tabs: [],
 });
 
-module.exports = mongoose.model('activities', schema);
+module.exports = mongoose.model('songs', schema);
