6be98acc657016d6b1a91a1d8380a1791c6a5205
Nicole Daniels <nzoner12@yahoo.com>
2015-10-10 03:03:44 -0400

    Fix Neels mistake

---
+++ client/components/footer/footer.directive.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('guitarStarApp')
+  .directive('footer', function () {
+    return {
+      templateUrl: 'components/footer/footer.html',
+      restrict: 'E',
+      link: function (scope, element) {
+        element.addClass('footer');
+      }
+    };
+  });
