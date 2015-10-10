05dcc073326c91e4fc973f39bf8b63d1697f2e83
Nicole Daniels <nzoner12@yahoo.com>
2015-10-10 14:38:39 -0400

    Add sharedProperties for cohesion across the application

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,10 +1,11 @@
 'use strict';
 angular.module('guitarStarApp')
-.controller('MainController',function($scope,$http,$timeout){
+.controller('MainController',function($scope,$http,$timeout,sharedProperties){
   var self = this;
   $scope.User = {};
 
   $scope.createUser = function(){
+    sharedProperties.setUsername($scope.User.name)
     $http.post('/startSong', $scope.User)
             .then(function(data) {
                 $scope.info = data;
@@ -14,20 +15,23 @@ angular.module('guitarStarApp')
 
   $scope.getTabs = function(){
     var requestUrl = '/getTabs?songName=' + $scope.User.songName;
+    sharedProperties.setSongName($scope.User.songName);
     $http.get(requestUrl)
     .then(function(response){
       $scope.tabs = response.data["tabs"];
+      sharedProperties.setTab($scope.tabs);
       showDrum();
     });
   }
 
   function showDrum (){
     $scope.counter = 0;
+    var array = sharedProperties.getTab();
     $scope.onTimeout = function(){
-      $scope.value = $scope.tabs[$scope.counter];
+      $scope.value = array[$scope.counter];
       console.log($scope.value);
       $scope.counter++;
-        if ($scope.counter < $scope.tabs.length) {
+        if ($scope.counter < array.length) {
             mytimeout = $timeout($scope.onTimeout,1000);
         }
     }
