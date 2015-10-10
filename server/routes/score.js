b728075c0154ab5369723dbbf4cf5b8b1e537e3e
Neel Kowdley <nkowdley@gmail.com>
2015-10-10 12:06:54 -0400

    scoring is done

--- server/routes/score.js
+++ server/routes/score.js
@@ -5,8 +5,11 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   //get the players name
   var name1=req.query['name'];
+  //get the song they are playing
+  var songName=req.query['songName'];
   //set the db
   var db=mongoose.connection;
+  //var user;
   //get the players actions
   db.collection('users').findOne({name:name1},function(err, user){
     if (err)
@@ -15,13 +18,37 @@ router.get('/', function(req, res, next) {
     }
     else
     {
-      res.send(user.actions);
+      //get the expected tabs
+      db.collection('tabs').findOne({name:songName},function(err, tab){
+        if (err)
+        {
+          return res.send(500, { error: err });
+        }
+        else
+        {
+          //Im sorry.  Really sorry.
+          //compare the users tabs to the expected tab output
+          var score=0;//initialize the score
+          //var PrintString='';
+          for(var i=0; i<user.actions.length; i++)
+          {
+            if (user.actions[i]==tab.tabs[i])
+            {
+              score++;
+            }
+            else
+            {
+              console.log(user.actions[i]+"!="+tab.tabs[i])
+            }
+            //PrintString+=user.actions[i];
+          }
+          res.send(score);
+        }
+      });
     }
   });
-  //compare the users tabs to the expected tab output
-  var score=0;//initialize the score
-
 });
+
 
 
 module.exports = router;
