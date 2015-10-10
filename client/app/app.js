f412cc04c555e74b247e3b520606eecf2ee8f55c
Nicole Daniels <nzoner12@yahoo.com>
2015-10-10 15:05:24 -0400

    Main page redirects to play page

--- client/app/app.js
+++ client/app/app.js
@@ -9,6 +9,14 @@ angular.module('guitarStarApp', [
 ])
   .config(function($routeProvider, $locationProvider) {
     $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainController'
+      })
+      .when('/play', {
+        templateUrl: 'app/drumset/drumset.html',
+        controller: 'DrumsetController'
+      })
       .otherwise({
         redirectTo: '/'
       });
--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,40 +1,18 @@
 'use strict';
 angular.module('guitarStarApp')
-.controller('MainController',function($scope,$http,$timeout,sharedProperties){
+.controller('MainController',function($scope,$http,$location,sharedProperties){
   var self = this;
   $scope.User = {};
 
   $scope.createUser = function(){
     sharedProperties.setUsername($scope.User.name)
+    sharedProperties.setSongName($scope.User.songName);
     $http.post('/startSong', $scope.User)
             .then(function(data) {
                 $scope.info = data;
                 console.log(data);
+                
             });
-  }
-
-  $scope.getTabs = function(){
-    var requestUrl = '/getTabs?songName=' + $scope.User.songName;
-    sharedProperties.setSongName($scope.User.songName);
-    $http.get(requestUrl)
-    .then(function(response){
-      $scope.tabs = response.data["tabs"];
-      sharedProperties.setTab($scope.tabs);
-      showDrum();
-    });
-  }
-
-  function showDrum (){
-    $scope.counter = 0;
-    var array = sharedProperties.getTab();
-    $scope.onTimeout = function(){
-      $scope.value = array[$scope.counter];
-      console.log($scope.value);
-      $scope.counter++;
-        if ($scope.counter < array.length) {
-            mytimeout = $timeout($scope.onTimeout,1000);
-        }
-    }
-    var mytimeout = $timeout($scope.onTimeout,1000);
+            $location.path('/play');
   }
 });
